458b8678640d9d1518f7a0fb57442cfd3c453c46
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-28 09:40:16 +0900

    [ feat ] 사용자 엔티티 개발 ( #1 )

---
+++ src/main/java/com/qring/auth/domain/model/UserEntity.java
@@ -0,0 +1,59 @@
+package com.qring.auth.domain.model;
+
+import com.qring.auth.domain.model.constraint.RoleType;
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_user")
+public class UserEntity {
+
+    @Id @Tsid
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private RoleType role;
+
+    @Column(name = "slack_email", nullable = false)
+    private String slackEmail;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "modified_at" , nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @Column(name = "deleted_by")
+    private String deletedBy;
+}
