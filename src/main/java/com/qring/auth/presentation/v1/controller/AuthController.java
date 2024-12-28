5b545ccebd6a931f88ad7f45f5fae8c602fcb5d3
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-28 10:55:36 +0900

    [ feat ] 사용자 회원 가입 API 개발 ( #1 )

---
+++ src/main/java/com/qring/auth/presentation/v1/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.qring.auth.presentation.v1.controller;
+
+import com.qring.auth.application.v1.res.AuthPostResDTOv1;
+import com.qring.auth.application.v1.res.ResDTO;
+import com.qring.auth.presentation.v1.req.PostAuthReqDTOv1;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @PostMapping("/v1/auth")
+    public ResponseEntity<ResDTO<AuthPostResDTOv1>> joinBy(PostAuthReqDTOv1 dto) {
+
+        return new ResponseEntity<>(
+                ResDTO.<AuthPostResDTOv1>builder()
+                        .code(HttpStatus.OK.value())
+                        .message("회원가입에 성공했습니다.")
+                        .data(AuthPostResDTOv1.of("tempUser", "tempRole", "temp0000@slack.com"))
+                        .build(),
+                HttpStatus.OK
+        );
+    }
+}
