57d117c5816ac6f40b282ccd4c08a43bae04f4d5
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-28 11:41:19 +0900

    [ feat ] Auth API 스웨거 문서화 추가 ( #1 )

--- src/main/java/com/qring/auth/presentation/v1/controller/AuthController.java
+++ src/main/java/com/qring/auth/presentation/v1/controller/AuthController.java
@@ -2,17 +2,19 @@ package com.qring.auth.presentation.v1.controller;
 
 import com.qring.auth.application.v1.res.AuthPostResDTOv1;
 import com.qring.auth.application.v1.res.ResDTO;
+import com.qring.auth.infrastructure.docs.AuthControllerSwagger;
 import com.qring.auth.presentation.v1.req.PostAuthReqDTOv1;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AuthController {
+public class AuthController implements AuthControllerSwagger {
 
     @PostMapping("/v1/auth")
-    public ResponseEntity<ResDTO<AuthPostResDTOv1>> joinBy(PostAuthReqDTOv1 dto) {
+    public ResponseEntity<ResDTO<AuthPostResDTOv1>> joinBy(@RequestBody PostAuthReqDTOv1 dto) {
 
         return new ResponseEntity<>(
                 ResDTO.<AuthPostResDTOv1>builder()
