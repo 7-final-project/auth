7c2b07bc98b23738b4de835559158bec7b0c8820
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-01 01:04:57 +0900

    [ feat ] RoleType 열거형에 문자열로부터 RoleType을 변환하는 fromString 메서드 추가 ( #3 )

--- src/main/java/com/qring/auth/domain/model/constraint/RoleType.java
+++ src/main/java/com/qring/auth/domain/model/constraint/RoleType.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum RoleType {
-    USER(Authority.CUSTOMER), // 사용자 권한
-    OWNER(Authority.OWNER), // 사장 권한
-    ADMIN(Authority.ADMIN); // 관리자 권한
+    USER(Authority.CUSTOMER),
+    OWNER(Authority.OWNER),
+    ADMIN(Authority.ADMIN);
 
     private final String authority;
 
@@ -16,6 +16,15 @@ public enum RoleType {
         public static final String CUSTOMER = "고객";
         public static final String OWNER = "점주";
         public static final String ADMIN = "관리자";
+    }
+
+    public static RoleType fromString(String authority) {
+        return switch (authority) {
+            case Authority.CUSTOMER -> RoleType.USER;
+            case Authority.OWNER -> RoleType.OWNER;
+            case Authority.ADMIN -> RoleType.ADMIN;
+            default -> throw new IllegalArgumentException("유효하지 않은 타입입니다.");
+        };
     }
 }
 
