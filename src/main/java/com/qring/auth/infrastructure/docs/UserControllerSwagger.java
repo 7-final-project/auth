8124f245df8871e72908c6962c00672b8c0cc05c
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-01 02:42:46 +0900

    [ comment ] 회원가입 스웨거 설명 수정 ( #3 )

--- src/main/java/com/qring/auth/infrastructure/docs/UserControllerSwagger.java
+++ src/main/java/com/qring/auth/infrastructure/docs/UserControllerSwagger.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/v1/users")
 public interface UserControllerSwagger {
 
-    @Operation(summary = "회원 생성", description = "사용자의 계정, 비밀번호, 권한, 슬랙 이메일을 통해 회원가입을 하는 API 입니다.")
+    @Operation(summary = "회원 생성", description = "회원가입을 하는 API 입니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "회원가입 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
             @ApiResponse(responseCode = "400", description = "회원가입 실패.", content = @Content(schema = @Schema(implementation = ResDTO.class)))
