1d0a38bba30340a1fe4bbe7ae98167c1ae37ee0d
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-01 18:11:38 +0900

    [ feat ] Auth 서비스 로그인 API 스웨거 문서 작성 ( #3 )

---
+++ src/main/java/com/qring/auth/infrastructure/docs/AuthControllerSwagger.java
@@ -0,0 +1,29 @@
+package com.qring.auth.infrastructure.docs;
+
+import com.qring.auth.application.global.dto.ResDTO;
+import com.qring.auth.presentation.v1.req.PostAuthReqDTOV1;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "Auth", description = "로그인 인증 API")
+@RequestMapping("/v1/auth")
+public interface AuthControllerSwagger {
+
+    @Operation(summary = "로그인", description = "로그인을 하는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
+            @ApiResponse(responseCode = "400", description = "로그인 실패.", content = @Content(schema = @Schema(implementation = ResDTO.class)))
+    })
+    @PostMapping("/login")
+    ResponseEntity<ResDTO<Object>> loginBy(HttpServletResponse res, @Valid @RequestBody PostAuthReqDTOV1 dto);
+}
--- src/main/java/com/qring/auth/presentation/v1/controller/AuthControllerV1.java
+++ src/main/java/com/qring/auth/presentation/v1/controller/AuthControllerV1.java
@@ -2,6 +2,7 @@ package com.qring.auth.presentation.v1.controller;
 
 import com.qring.auth.application.global.dto.ResDTO;
 import com.qring.auth.application.v1.service.AuthServiceV1;
+import com.qring.auth.infrastructure.docs.AuthControllerSwagger;
 import com.qring.auth.infrastructure.jwt.JwtUtil;
 import com.qring.auth.presentation.v1.req.PostAuthReqDTOV1;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/auth")
-public class AuthControllerV1 {
+public class AuthControllerV1 implements AuthControllerSwagger {
 
     private final AuthServiceV1 authServiceV1;
 
