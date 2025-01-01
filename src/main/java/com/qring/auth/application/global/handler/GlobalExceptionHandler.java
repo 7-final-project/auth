e43993cb46502a8674d1401f0388fa558916a6f0
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-01 18:04:45 +0900

    [ refactor ] 커스텀 예외들의 부모 예외인 AuthException을 글로벌 예외로 통합하여 중복 코드 제거 ( #3 )

--- src/main/java/com/qring/auth/application/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/qring/auth/application/global/handler/GlobalExceptionHandler.java
@@ -1,9 +1,7 @@
 package com.qring.auth.application.global.handler;
 
 import com.qring.auth.application.global.dto.ResDTO;
-import com.qring.auth.application.global.exception.DuplicateResourceException;
-import com.qring.auth.application.global.exception.UnauthorizedAccessException;
-import com.qring.auth.application.global.exception.ValidationException;
+import com.qring.auth.application.global.exception.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,30 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({UnauthorizedAccessException.class})
-    public ResponseEntity<ResDTO<Object>> unauthorizedAccessExceptionHandler(UnauthorizedAccessException ex) {
-        return new ResponseEntity<>(
-                ResDTO.builder()
-                        .code(ex.getErrorCode().getCode())
-                        .message(ex.getMessage())
-                        .build(),
-                ex.getErrorCode().getHttpStatus()
-        );
-    }
-
-    @ExceptionHandler({DuplicateResourceException.class})
-    public ResponseEntity<ResDTO<Object>> duplicateResourceExceptionHandler(DuplicateResourceException ex) {
-        return new ResponseEntity<>(
-                ResDTO.builder()
-                        .code(ex.getErrorCode().getCode())
-                        .message(ex.getMessage())
-                        .build(),
-                ex.getErrorCode().getHttpStatus()
-        );
-    }
-
-    @ExceptionHandler({ValidationException.class})
-    public ResponseEntity<ResDTO<Object>> validationExceptionHandler(ValidationException ex) {
+    @ExceptionHandler({AuthException.class})
+    public ResponseEntity<ResDTO<Object>> authExceptionHandler(AuthException ex) {
         return new ResponseEntity<>(
                 ResDTO.builder()
                         .code(ex.getErrorCode().getCode())
